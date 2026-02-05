fd25fea9c6642c7b7c8ca3ca621cd126013bb7b0
kovalev.e <kovalev.e@web-canape.ru>
2026-02-05 15:40:41 +0300

    init

---
+++ src/components/CinemaSeatBooking.jsx
@@ -0,0 +1,14 @@
+const CinemaSeatBooking = () => {
+  return (
+    <div>
+      {/* title */}
+      {/* screen */}
+      {/* seat map */}
+      {/* legend */}
+      {/* summary */}
+      {/* book button */}
+    </div>
+  );
+};
+
+export default CinemaSeatBooking;
